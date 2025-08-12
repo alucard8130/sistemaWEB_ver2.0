e93703b20ef65601e94cc5347f83c1ba70013651
Alucard8130 <smart8130@hotmail.com>
2025-08-11 20:24:57 -0600

    update

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 
-import debug_toolbar
+#import debug_toolbar
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -85,7 +85,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ]
+    # urlpatterns += [
+    #     path('__debug__/', include(debug_toolbar.urls)),
+    # ]
     #urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
